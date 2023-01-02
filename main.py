361b8057cba880536ce09386d91ee6a0dd27ca74
Nicolas LEE <leenicolas103@gmail.com>
2023-01-02 16:01:31 -0300

    clean code to understand like pseudo code

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
-N = 21
-j1 = input("entrer le nom du joueur 1")
-j2 = input("entrer le nom du joueur 2")
-while(N >= 1):
-    print("Allumettes disponibles: " + " | " * N)
+def player_one_turn(N, j1):
+  print("c'est à",j1,"de jouer")
+  nb_allumettes_joueur = -1
+  while(not nb_allumettes_joueur in range(1,4)):
+    nb_allumettes_joueur = int(input("Combien d'allumettes souhaitez vous retirer ? (entre 1 et 3) : "))
+  N = N - nb_allumettes_joueur
+  if(N <= 0):
+    print(j1,"Vous avez perdu !")
+    return 0
+  elif(N == 1):
+    print(j1,"Vous avez gagné !")
+    return 1
+  else:
+    return N
 
-    print("c'est à",j1,"de jouer")
-    nb_allumettes_joueur = -1
-    while(not nb_allumettes_joueur in range(1,4)):
-        nb_allumettes_joueur = int(input("Combien d'allumettes souhaitez vous retirer ? (entre 1 et 3) : "))
+
+def player_two_turn(N,j2):
+  print("c'est à",j2,"de jouer")
+  nb_allumettes_joueur = -1
+  while(not nb_allumettes_joueur in range(1,4)):
+    nb_allumettes_joueur = int(input("Combien d'allumettes souhaitez vous retirer ? (entre 1 et 3) : "))
     N = N - nb_allumettes_joueur
-    if(N < 0):
-       print(j1,"Vous avez perdu !")
-       break;
+    if(N <= 0):
+      print(j2,"Vous avez perdu !")
+      return 0
     elif(N == 1):
-        print(j1,"Vous avez gagné !")
-        break;
+      print(j2,"Vous avez gagné !")
+      return 1
+    else:
+      return N
+
+
+def game():
+  N = 21
+  j1 = input("entrer le nom du joueur 1")
+  j2 = input("entrer le nom du joueur 2")
+  while(N >= 1):
     print("Allumettes disponibles: " + " | " * N)
-    print("c'est à",j2,"de jouer")
-    nb_allumettes_joueur = -1
-    while(not nb_allumettes_joueur in range(1,4)):
-        nb_allumettes_joueur = int(input("Combien d'allumettes souhaitez vous retirer ? (entre 1 et 3) : "))
-    N = N - nb_allumettes_joueur
-    if(N < 0):
-       print(j2,"Vous avez perdu !")
+    t1 = player_one_turn(N,j1)
+    if t1 <= 0 or t1 == 1:
        break;
-    elif(N == 1):
-        print(j2,"Vous avez gagné !")
-        break;
-    
+    elif (t1 != 0 and t1 != 1):
+      N = t1
+    print("Allumettes disponibles: " + " | " * N)
+    t2 = player_two_turn(N,j2)
+    if t2 <= 0 or t2 == 1:
+       break;
+    elif (t2 != 0 and t2 != 1):
+      N = t2
+game()
