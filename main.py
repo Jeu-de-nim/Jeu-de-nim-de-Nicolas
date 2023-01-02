fec92da938ff7aff4748120b20658ee7e89370f1
Nicolas LEE <leenicolas103@gmail.com>
2023-01-02 12:37:59 -0300

    first push

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-test
+N = 21
+j1 = input("entrer le nom du joueur 1")
+j2 = input("entrer le nom du joueur 2")
+while(N >= 1):
+    print("Allumettes disponibles: " + " | " * N)
+
+    print("c'est à",j1,"de jouer")
+    nb_allumettes_joueur = -1
+    while(not nb_allumettes_joueur in range(1,4)):
+        nb_allumettes_joueur = int(input("Combien d'allumettes souhaitez vous retirer ? (entre 1 et 3) : "))
+    N = N - nb_allumettes_joueur
+    if(N < 0):
+       print(j1,"Vous avez perdu !")
+       break;
+    elif(N == 1):
+        print(j1,"Vous avez gagné !")
+        break;
+    print("Allumettes disponibles: " + " | " * N)
+    print("c'est à",j2,"de jouer")
+    nb_allumettes_joueur = -1
+    while(not nb_allumettes_joueur in range(1,4)):
+        nb_allumettes_joueur = int(input("Combien d'allumettes souhaitez vous retirer ? (entre 1 et 3) : "))
+    N = N - nb_allumettes_joueur
+    if(N < 0):
+       print(j2,"Vous avez perdu !")
+       break;
+    elif(N == 1):
+        print(j2,"Vous avez gagné !")
+        break;
+    
