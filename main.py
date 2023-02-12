08ddf910e876e0dc218056fdbaf042ce3f65008a
Nicolas LEE <leenicolas103@gmail.com>
2023-02-12 17:30:43 -0300

    added AI

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def player_one_turn(N, j1):
   print("c'est à",j1,"de jouer")
   nb_allumettes_joueur = -1
@@ -29,22 +31,56 @@ def player_two_turn(N,j2):
     else:
       return N
 
+def ordinateur_turn(N):
+  print("Tour de l'ordinateur")
+  nb_allumettes_ordinateur = 0
+  if(N <= 4):
+    nb_allumettes_ordinateur = N - 1
+    print("Vous avez perdu. L'ordinateur prends {} allumettes.".format(nb_allumettes_ordinateur))
+    print("Vous prenez donc forcément la dernière")
+    return N
+  else:
+    nb_allumettes_ordinateur = N % 6
+    if(nb_allumettes_ordinateur == 0):
+      nb_allumettes_ordinateur = random.randint(1, 3)
+      print("L'ordinateur prends {} allumettes.".format(nb_allumettes_ordinateur))
+    N = N - nb_allumettes_ordinateur
+    return N
 
 def game():
   N = 21
-  j1 = input("entrer le nom du joueur 1:")
-  j2 = input("entrer le nom du joueur 2:")
-  while(N >= 1):
-    print("Allumettes disponibles: " + " | " * N)
-    t1 = player_one_turn(N,j1)
-    if t1 <= 0 or t1 == 1:
-       break;
-    elif (t1 != 0 and t1 != 1):
-      N = t1
-    print("Allumettes disponibles: " + " | " * N)
-    t2 = player_two_turn(N,j2)
-    if t2 <= 0 or t2 == 1:
-       break;
-    elif (t2 != 0 and t2 != 1):
-      N = t2
+  a = input("taper 1 pour jouer solo, taper 2 pour jouer a deux:")
+  if (a == "2"):
+    j1 = input("entrer le nom du joueur 1:")
+    j2 = input("entrer le nom du joueur 2:")
+    while(N >= 1):
+      print("Allumettes disponibles: " + " | " * N)
+      t1 = player_one_turn(N,j1)
+      if t1 <= 0 or t1 == 1:
+        break;
+      elif (t1 != 0 and t1 != 1):
+        N = t1
+      print("Allumettes disponibles: " + " | " * N)
+      t2 = player_two_turn(N,j2)
+      if t2 <= 0 or t2 == 1:
+        break;
+      elif (t2 != 0 and t2 != 1):
+        N = t2
+  elif (a == "1"):
+    j1 = input("entrer le nom du joueur 1:")
+    while(N >= 1):
+      print("Allumettes disponibles: " + " | " * N)
+      t1 = player_one_turn(N,j1)
+      if t1 <= 0 or t1 == 1:
+        break;
+      elif (t1 != 0 and t1 != 1):
+        N = t1
+      print("Allumettes disponibles: " + " | " * N)
+      bt = ordinateur_turn(N)
+      if (bt <= 0 or bt == 1):
+        break;
+      elif (bt != 0 and bt != 1):
+        N = bt
+  else:
+    print("erreur")
 game()
